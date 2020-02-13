4016390cab497a4100ed81b8cbeeb01627333dc4
XMY7594 <betzabemoraima.ojeda.contractor@bbva.com>
2020-02-13 09:16:01 -0600

    Se agrega header y footer

---
+++ app/scripts/lit-initial-components.js
@@ -0,0 +1,7 @@
+// Import here your LitElement initial components (critical / startup)
+
+// Auto generated imports below. DO NOT remove!
+// will be replaced with imports
+// ${filledByCellsWithAutoImports}
+import '../pages//another-page/another-page.js';
+import '../pages//home-page/home-page.js';
